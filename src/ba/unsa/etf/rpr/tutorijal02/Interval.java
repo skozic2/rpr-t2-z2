aed3aa06b77ab91c313c2ae0aa054195ed71a5bc
student <skozic2>
2019-10-21 18:41:32 +0200

    zadatak

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,33 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    private double p, k;
+    private boolean pp, kp;
+    public Interval(double x1, double x2, boolean b1, boolean b2){
+        if(x1>x2) throw IllegalArgumentException//izbaciti izuzetak
+        p=x1;
+        k=x2;
+        pp=b1;
+        kp=b2;
+    }
+    public Interval(){
+        p=0;
+        k=0;
+        pp=false;
+        kp=false;
+    }
+    public boolean isNull(){
+        if(p==0 && k==0 && pp==false && kp==false) return true;
+        return false;
+    }
+    public boolean isIn(double t){
+        if ((t>p && t<k)||(t==k && kp==true) || (t==p && pp=true)) return true;
+        return false;
+    }
+    public Interval intersect(Interval i){
+
+    }
+
 
 
 }
