8aac96ca510bd40eaded2b5179e183784ff59a46
Sarah Kozic <skozic2@etf.unsa.ba>
2019-10-24 19:48:38 +0200

    zavrsen zadatak

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -4,7 +4,7 @@ public class Interval {
     private double p, k;
     private boolean pp, kp;
     public Interval(double x1, double x2, boolean b1, boolean b2){
-        if(x1>x2) throw IllegalArgumentException//izbaciti izuzetak
+        if(x1>x2) throw new IllegalArgumentException("");
         p=x1;
         k=x2;
         pp=b1;
@@ -16,18 +16,82 @@ public class Interval {
         pp=false;
         kp=false;
     }
+    public double getP(){return p;}
+    public double getK(){return k;}
+    public boolean getPP(){ return pp;}
+    public boolean getKP(){ return kp;}
+
     public boolean isNull(){
-        if(p==0 && k==0 && pp==false && kp==false) return true;
+        if(p==0 && k==0 && !pp && !kp) return true;
         return false;
     }
     public boolean isIn(double t){
-        if ((t>p && t<k)||(t==k && kp==true) || (t==p && pp=true)) return true;
+        if ((t>p && t<k)||(t==k && kp) || (t==p && pp)) return true;
         return false;
     }
     public Interval intersect(Interval i){
-
+        double poc, kr;
+        boolean ppoc=false , pkr=false;
+        if(i.getP()>this.k || i.getK()<this.p) return new Interval();
+        if(i.getP()>=this.p){
+            if(i.getPP())  ppoc=true;
+            poc=i.getP();
+        }
+        else{
+            poc=this.p;
+            if(this.pp) ppoc=true;
+        }
+        if(i.getK()<=this.k) {
+            if(i.getKP()) pkr=true;
+            kr=i.getK();
+        }
+        else{
+            if(this.kp) pkr=true;
+            kr=this.k;
+        }
+        return new Interval(poc,kr,ppoc,pkr);
     }
-
+    public static Interval intersect(Interval i, Interval j){
+        double poc, kr;
+        boolean ppoc=false , pkr=false;
+        if(i.getP()>j.getK() || i.getK()<j.getP()) return new Interval();
+        if(i.getP()>=j.getP()){
+            if(i.getPP())  ppoc=true;
+            poc=i.getP();
+        }
+        else{
+            poc=j.getP();
+            if(j.getPP()) ppoc=true;
+        }
+        if(i.getK()<=j.getK()) {
+            if(i.getKP()) pkr=true;
+            kr=i.getK();
+        }
+        else{
+            if(j.getKP()) pkr=true;
+            kr=j.getK();
+        }
+        return new Interval(poc,kr,ppoc,pkr);
+    }
+    @Override
+    public boolean equals(Object j){
+        Interval i=(Interval)j;
+        return (i.getP()==this.p && i.getK()==this.k && i.getPP()==this.pp && i.getKP()==this.kp);
+    }
+    @Override
+    public String toString(){
+        //double p=this.getP(), k=this.getK();
+        //boolean pp=this.getPP(), kp=this.getKP();
+        if(this.isNull()) return "()";
+        if(pp){
+            if(kp) return ("["+p+","+k+"]");
+            return ("["+p+","+k+")");
+        }
+        else{
+            if(kp) return ("("+p+","+k+"]");
+            return ("("+p+","+k+")");
+        }
+    }
 
 
 }
--- src/ba/unsa/etf/rpr/tutorijal02/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Main.java
@@ -2,7 +2,5 @@ package ba.unsa.etf.rpr.tutorijal02;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Nema ništa u mainu, pokrenite testove!");
-    }
+    public static void main(String[] args) { System.out.println("Nema ništa u mainu, pokrenite testove!"); }
 }
